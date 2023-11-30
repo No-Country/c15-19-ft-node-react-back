9c0ce763938ab01f2b2c9edbceb4138b50f57a43
HX-NBurgueno <nburgueno@soyhenry.com>
2023-11-30 16:44:19 -0300

    create challenge and user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -3,10 +3,69 @@ const { Schema, model } = require("mongoose");
 const UserSchema = new Schema({
   name: {
     type: String,
+    required: true,
   },
   email: {
     type: String,
+    unique: true,
+    required: true,
   },
+  lastname: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    unique: true
+  },
+  picture: {
+    type: Buffer
+  },
+  role: {
+    type: String,
+    enum: ["admin", "challenger"],
+    default: "challenger"
+  },
+  followers: [{
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    followDate: {
+      type: Date,
+      default: new Date.now()
+    }
+  }],
+  following: [{
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    },
+    followDate: {
+      type: Date,
+      default: new Date.now()
+    }
+  }],
+  disabled: {
+    type: Boolean,
+    default: false
+  },
+  verificationEmail: {
+    type: Boolean,
+    default: false
+  },
+  createdAt: {
+    type: Date,
+    default: new Date.now()
+  },
+  updatedAt: {
+    type: Date,
+    default: new Date.now()
+  }
 });
 
-module.ewxports = model("User", UserSchema);
+module.exports = model("User", UserSchema);
